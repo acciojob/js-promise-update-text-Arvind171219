f6fc87df2311830ebbb9488e9a8f8d5599882fca
Arvind kumar <74968170+Arvind171219@users.noreply.github.com>
2023-06-22 11:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+function updateElementText() {
+  const outputElement = document.getElementById("output");
+  
+  updateOutputText().then((message) => {
+    outputElement.textContent = message;
+  });
+}
